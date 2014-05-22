548f0c819350a1a017c49ea5594462ec93dac4d8
LobaSakaO <b99901099@ntu.edu.tw>
2014-05-22 14:45:23 +0800

    2

--- catcheya.js
+++ catcheya.js
@@ -1,41 +1,45 @@
 var cheerio = require('cheerio');
 var request = require('request');
 
-var appleUrl = 'http://www.appledaily.com.tw/realtimenews/section/new/';
+const appleUrl = 'http://www.appledaily.com.tw/realtimenews/section/new/';
 
 var newsList;
-var tmp_cate = {"category":"", "news_count":0, "news":[]};
-var tmp_news = {"title":"", "url":"", "time":"", "video":""};
-var allCategory = ['動物','FUN','瘋啥','搜奇','正妹','體育','臉團','娛樂','時尚','生活','社會','國際','財經','地產','政治','論壇'];
+
+function tmp_cate(category){
+    return {"category":category, "news_count":0, "news":[]};
+}
+function tmp_news(title, url, time, video){
+    return {"title":title, "url":url, "time":time, "video":video};
+}
+
+const allCategory = ['動物','FUN','瘋啥','搜奇','正妹','體育','臉團','娛樂','時尚','生活','社會','國際','財經','地產','政治','論壇'];
 var output = {};
-allCategory.forEach(function(cate, index, array){
-	var tmp_c = tmp_cate;
-	tmp_c['category'] = cate;
-	output[cate] = tmp_c;
+
+allCategory.forEach(function(element, index, array){
+	output[element] = new tmp_cate(element);
+    console.log(output);
 });
-console.log(output);
 
 request.get(appleUrl+'1', function(err, res, html){
 	if (!err && res.statusCode == 200) {
-    var $ = cheerio.load(html);
-    newsList = $("#maincontent div.abdominis > ul.rtddd.slvl > li.rtddt");
 
-    newsList.each(function(){
-    	var theCate = $(this).find("h2").text();
-    	var theTitle = $(this).find("h1 > font").text();
-    	var theTime = $(this).find("time").text();
-    	// var theUrl = $(this).find("a").href();
+        var $ = cheerio.load(html);
+        newsList = $("#maincontent div.abdominis > ul.rtddd.slvl > li.rtddt");
 
-    	var tmp_n = tmp_news;
-    	tmp_n['title'] = theTitle;
+        newsList.each(function(){
+        	var theCate = $(this).find("h2").text();
+        	var theTitle = $(this).find("h1 > font").text();
+        	var theTime = $(this).find("time").text();
+        	// var theUrl = $(this).find("a").href();
 
-    	output[theCate]['news'] = tmp_n;
-    	console.log(output["動物"]);
-    });
 
-    // mainUl.find("li .rtddd").each(function(){
-    // 	console.log($(this).attr());
-    // });
-  }
+        	output[theCate]['news'].push(new tmp_news(theTitle, '', theTime, false));
+
+        });
+
+        console.log(output);
+        console.log(JSON.stringify(output), null, 4);
+
+    }
 });
 
