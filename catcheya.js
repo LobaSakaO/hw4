2981683fa9657e6ee93ea8a4145c722134ff2604
LobaSakaO <b99901099@ntu.edu.tw>
2014-05-24 23:07:29 +0800

    done

--- catcheya.js
+++ catcheya.js
@@ -1,5 +1,6 @@
 var cheerio = require('cheerio');
 var request = require('request');
+var Q = require('q');
 var fs = require('fs');
 
 const appleUrl = 'http://www.appledaily.com.tw/realtimenews/section/new/';
@@ -19,24 +20,24 @@ allCategory.forEach(function(element, index, array){
 	output[element] = new tmp_cate(element);
 });
 
-var doneNum = 0;
-request.get(appleUrl+1, function(err, res, html){
-    if (!err && res.statusCode == 200) {
-        var $ = cheerio.load(html);
-        latestDate = $("#maincontent div.abdominis > h1.dddd > time").text();
-        console.log('aa',latestDate);
-        doneNum += 1;
-    }
-    else
-    {
-        console.log('request failed');
-    }
-});
+function getLatestDate(){
+    var deferred = Q.defer();
+    
+    request.get(appleUrl+1, function(err, res, html){
+        if (!err && res.statusCode == 200) {
+            var $ = cheerio.load(html);
+            latestDate = $("#maincontent div.abdominis > h1.dddd > time").text();
+        }
+        console.log('latestDate',latestDate);
+        deferred.resolve();
+    });
+    return deferred.promise;
+    
+}
 
-
-for (var page = 1; page<=5; page++)
+function getNews(page)
 {
-    console.log('page is', page);
+    var deferred = Q.defer();
     request.get(appleUrl+page, function(err, res, html){
     	if (!err && res.statusCode == 200) {
 
@@ -49,36 +50,41 @@ for (var page = 1; page<=5; page++)
             	var theTime = $(this).find("time").text();
                 var theUrl = $(this).find("a").attr('href');
                 var hasVideo = $(this).hasClass('hsv');
-
             	output[theCate]['news'].push(new tmp_news(theTitle, theUrl, theTime, hasVideo));
-
             });
-            doneNum += 1;
-        }
 
-    });
-
+        }   //if
+        console.log('done page ' + page);
+        deferred.resolve();
+    }); //request
+    return deferred.promise;
 }
 
-while(doneNum < 6)
-{
-    console.log(doneNum);
-}
 
-for (var cate in output){
-    output[cate]['news_count'] = output[cate]['news'].length;
-}
-var maxNews = '';
-var maxNewsCount = 0;
-for (var cate in output){
-    if (output[cate]['news_count'] > maxNewsCount)
-    {
-        maxNewsCount = output[cate]['news_count'];
-        maxNews = output[cate]['category'];
+function generateFinalOutput(){
+    for (var cate in output){
+        output[cate]['news_count'] = output[cate]['news'].length;
     }
+    var maxNews = '';
+    var maxNewsCount = 0;
+    for (var cate in output){
+        if (output[cate]['news_count'] > maxNewsCount)
+        {
+            maxNewsCount = output[cate]['news_count'];
+            maxNews = output[cate]['category'];
+        }
+    }
+
+    var finalOutput = [];
+    for (var cate in output)
+    {
+        finalOutput.push(output[cate]);
+    }
+    console.log(JSON.stringify(finalOutput, null, 4));
+    fs.writeFile('appledaily.json', finalOutput);
+    console.log(latestDate + " - 新聞數量最多的分類為[" + maxNews + "], 共有" + maxNewsCount + "則新聞");
 }
 
-console.log(JSON.stringify(output, null, 4));
-console.log(latestDate.text() + " - 新聞數量最多的分類為[" + maxNews + "], 共有" + maxNewsCount + "則新聞");
+Q.allSettled([getLatestDate(), getNews(1), getNews(2), getNews(3), getNews(4), getNews(5)]).then(generateFinalOutput).done();
 
 
